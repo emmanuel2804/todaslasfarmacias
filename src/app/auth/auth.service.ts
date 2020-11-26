0273b6d427d26c5bff8203522c8f3435036293c0
Emilio Avalos <eoavals@gmail.com>
2020-11-26 00:12:52 -0600

    Token local persistence removed

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class AuthService {
   private token: string;
-  private tokenTimer: any;
 
   constructor(private http: HttpClient) {}
 
@@ -19,43 +18,10 @@ export class AuthService {
       )
       .subscribe((response) => {
         this.token = response.token;
-
-        // if (this.token) {
-        //   this.setAuthTimer(response.expiresIn.milliseconds);
-        //   this.saveAuthData(this.token, response.expiresIn.date);
-        // }
       });
   }
 
   public getToken(): string {
     return this.token;
-  }
-
-  // Saves token to the local storage
-  private saveAuthData(token: string, date: any): void {
-    localStorage.setItem('tlfToken', token);
-    localStorage.setItem('tlfExpirationDate', date);
-  }
-
-  // Gets token from the local storage
-  private getAuthData(): { token: string; expirationDate: Date } {
-    const token = localStorage.getItem('tlfToken');
-    const expirationDate = localStorage.getItem('tlfExpirationDate');
-
-    if (!token || !expirationDate) return;
-
-    return {
-      token: token,
-      expirationDate: new Date(expirationDate),
-    };
-  }
-
-  // Sets a timer to the time left for the token to expire
-  // if expired requests a new one (by login the user)
-  private setAuthTimer(duration: number): void {
-    this.tokenTimer = setTimeout(() => {
-      // logs back the user once the time has run out (token has expired)
-      this.autoLogin();
-    }, duration);
   }
 }
